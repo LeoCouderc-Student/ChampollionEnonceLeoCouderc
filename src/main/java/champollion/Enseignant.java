f0d324cc14b753466a354d314608762e1a79a5b5
leocouderc <leo.coud@gmail.com>
2021-03-01 19:32:59 +0100

    done

--- src/main/java/champollion/Enseignant.java
+++ src/main/java/champollion/Enseignant.java
@@ -1,11 +1,24 @@
 package champollion;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public class Enseignant extends Personne {
 
     // TODO : rajouter les autres méthodes présentes dans le diagramme UML
 
+	private Map<Intervention, UE> interventionPlanifiees;
+	private Map<UE, ServicePrevu> enseignements;
+
     public Enseignant(String nom, String email) {
         super(nom, email);
+        
+        this.interventionPlanifiees = new HashMap<Intervention, UE>();
+        this.enseignements = new HashMap<UE, ServicePrevu>();
     }
 
     /**
@@ -17,8 +30,12 @@ public class Enseignant extends Personne {
      *
      */
     public int heuresPrevues() {
-        // TODO: Implémenter cette méthode
-        throw new UnsupportedOperationException("Pas encore implémenté");
+    	float resultat = 0F;
+    	
+    	for (ServicePrevu service: this.enseignements.values()) {
+    		resultat += service.getVolumeCM() * 1.5F + service.getVolumeTD() + service.getVolumeTP() * 0.75F;
+		}
+        return (int) resultat;
     }
 
     /**
@@ -31,8 +48,8 @@ public class Enseignant extends Personne {
      *
      */
     public int heuresPrevuesPourUE(UE ue) {
-        // TODO: Implémenter cette méthode
-        throw new UnsupportedOperationException("Pas encore implémenté");
+    	ServicePrevu specifiedService = this.enseignements.get(ue);
+        return (int) (specifiedService.getVolumeCM() * 1.5F + specifiedService.getVolumeTD() + specifiedService.getVolumeTP() * 0.75F);
     }
 
     /**
@@ -44,8 +61,45 @@ public class Enseignant extends Personne {
      * @param volumeTP le volume d'heures de TP
      */
     public void ajouteEnseignement(UE ue, int volumeCM, int volumeTD, int volumeTP) {
-        // TODO: Implémenter cette méthode
-        throw new UnsupportedOperationException("Pas encore implémenté");
+    		
+    	if(!this.enseignements.containsKey(ue)) {
+    		ServicePrevu service = new ServicePrevu(volumeTD, volumeTP, volumeCM);
+    		this.enseignements.put(ue, service);
+    	} else {
+    		ServicePrevu service = this.enseignements.get(ue);
+    		service.setVolumeCM(volumeCM + service.getVolumeCM());
+    		service.setVolumeTD(volumeTD + service.getVolumeTD());
+    		service.setVolumeTP(volumeTP + service.getVolumeTP());
+    	}
+    }
+    
+    /**
+     * Détermine si un enseignant a fait moins de 192h
+     */
+    public boolean enSousService() {
+    	return this.heuresPrevues() < 192;
+    }
+    
+    /**
+     * ajoute une intervention planifiée pour cet enseignant
+     */
+    public void ajouteIntervention(UE ue, Intervention intervention) {
+    	this.interventionPlanifiees.put(intervention, ue);
+    }
+    
+    /**
+     * Nombre d'heure restante à faire pour une eu et un type donnée
+     */
+    public int resteAPlanifier(UE ue, TypeIntervention type) {
+    	float heureFaite = 0F;
+    	List<Intervention> list = new ArrayList<Intervention>(this.interventionPlanifiees.keySet());
+    	for (Intervention intervention : list)
+    	{
+			if ( !intervention.getAnnulee() && this.interventionPlanifiees.get(intervention).equals(ue) && type.equals(intervention.getType())) {
+				heureFaite += intervention.getDuree();
+			}
+		}
+    	return (int) (this.heuresPrevuesPourUE(ue) - heureFaite);
     }
 
 }
--- src/test/java/champollion/ChampollionJUnitTest.java
+++ src/test/java/champollion/ChampollionJUnitTest.java
@@ -3,15 +3,20 @@ package champollion;
 import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.Date;
+
 public class ChampollionJUnitTest {
 	Enseignant untel;
 	UE uml, java;
+	Intervention tdUml;
 		
 	@BeforeEach
 	public void setUp() {
 		untel = new Enseignant("untel", "untel@gmail.com");
 		uml = new UE("UML");
-		java = new UE("Programmation en java");		
+		java = new UE("Programmation en java");	
+		Salle A111 = new Salle("A111", 20);
+		tdUml = new Intervention(new Date(), 1, false, 10, TypeIntervention.TD, A111);
 	}
 	
 
@@ -34,7 +39,19 @@ public class ChampollionJUnitTest {
                 
 		assertEquals(10 + 20, untel.heuresPrevuesPourUE(uml),
                          "L'enseignant doit maintenant avoir 30 heures prévues pour l'UE 'uml'");		
-		
+	}
+	
+	@Test
+	public void testEnSousService() {
+		untel.ajouteEnseignement(uml, 1, 2, 3);
+		assertTrue(untel.enSousService());
+	}
+	
+	@Test
+	public void testResteAPlanifier() {
+		untel.ajouteEnseignement(uml, 0, 5, 0);
+		untel.ajouteIntervention(uml, tdUml);
+		assertEquals(4, untel.resteAPlanifier(uml, TypeIntervention.TD));
 	}
 	
 }
